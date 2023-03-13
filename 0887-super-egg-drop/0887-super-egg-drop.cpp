605d13bb476de6e422ed01969e20dfc640b00140
Abhay Dixit <adixit7386@gmail.com>
2023-03-13 17:53:19 +0530

    Time: 1126 ms (5.15%), Space: 47.2 MB (15.13%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,50 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-
-    int superEggDrop(int e, int f) {
+    int superEggDrop(int k, int n) {
+        // if(k==1){
+        //     return n;
+        // }
+        // if(n<=1){
+        //     return n;
+        // }
+        // int res=n+1;
+        // for(int i=1;i<=n;i++){
+        //     res=min(res,1+max(superEggDrop(k-1,i-1),superEggDrop(k,n-i)));
+        // }
+        // return res;
         
-        vector<vector<int>> dp(e+1,vector<int> (f+1,f));
-        
-        for(int i=0;i<=f;i++){
-            dp[1][i]=i;
-            
+        vector<vector<int>> dp(n+1,vector<int> (k+1,n+1));
+        for(int i=0;i<=k;i++){
+            dp[0][i]=0;
+            dp[1][i]=1;
         }
-        for(int i=0;i<=e;i++){
-            dp[i][1]=1;
-            dp[i][0]=0;
-            
+        for(int i=0;i<=n;i++){
+            dp[i][1]=i;
         }
         
-       
-        
-        
-        
-        for(int i=2;i<=e;i++){
-            for(int j=2;j<=f;j++){
+        for(int i=2;i<=n;i++){
+            for(int j=2;j<=k;j++){
+                dp[i][j]=n+1;
+                // for(int l=1;l<=i;l++){
+                //    dp[i][j]=min(dp[i][j],1+max(dp[l-1][j-1],dp[i-l][j])) ;
+                // }
                 
-                int l=1,h=j,temp=0,ans=100000;
-                 while(l<=h)
-                 {
-                    int mid=(l+h)/2;
-                    int left= dp[i-1][mid-1]; //egg broken check for down floors of mid
-                    int right= dp[i][j-mid];  // not broken check for up floors of mid
-                    temp=1+max(left,right);          //store max of both 
-                    if(left<right){                  //since right is more than left and we need more in worst case 
-                      l=mid+1;                       // so l=mid+1 to gain more temp for worst case : upward
+                int low=1,high=i;
+                while(low<=high){
+                    int mid=(low+high)/2;
+                    
+                    int left=dp[mid-1][j-1];
+                    int right=dp[i-mid][j];
+                    if(left<right){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
                     }
-                    else                             //left > right so we will go downward 
-                    {    
-                        h=mid-1;
-                    }
-                    ans=min(ans,temp);               //store minimum attempts
-                 }
-                 dp[i][j]=ans;
+                    dp[i][j]=min(dp[i][j],(1+max(left,right)));
+                }
             }
         }
-        
-        return dp[e][f];
-        
-        
-        
+        return dp[n][k];
     }
 };
