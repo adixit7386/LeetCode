1fce85c93ba0aeb10ca73cf564df8ee51d049a1e
Abhay Dixit <adixit7386@gmail.com>
2023-04-08 19:11:40 +0530

    Time: 3 ms (80.28%), Space: 13.1 MB (59.15%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -12,19 +12,10 @@
 class Solution {
 public:
     void uniquePaths(TreeNode* root,string str,vector<string> &ans){
-        if(root==NULL){
-            return;
-        }
-        // string str=to_string(root->val);
-        if(!root->left && !root->right){
-            ans.push_back(str);
-        }
-        if(root->left!=NULL){
-            uniquePaths(root->left,str+"->"+to_string(root->left->val),ans);
-        }
-        if(root->right!=NULL){
-            uniquePaths(root->right,str+"->"+to_string(root->right->val),ans);
-        }
+        if(root==NULL)return;
+        if(!root->left && !root->right)ans.push_back(str);
+        if(root->left!=NULL)uniquePaths(root->left,str+"->"+to_string(root->left->val),ans);
+        if(root->right!=NULL)uniquePaths(root->right,str+"->"+to_string(root->right->val),ans);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
