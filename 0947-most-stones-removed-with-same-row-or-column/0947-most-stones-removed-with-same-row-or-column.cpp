2407d8e83c183ca2f3ef90882ac483f16a162f90
Abhay Dixit <adixit7386@gmail.com>
2023-04-08 14:01:23 +0530

    Time: 296 ms (23.73%), Space: 14.1 MB (97.07%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,73 +1,32 @@
-class DisjointSet{
-    vector<int> rank,parent,size;
-    public:
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        size.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    int findUPar(int node){
-        if(node==parent[node]){
-            return node;
-        }
-        return parent[node]=findUPar(parent[node]);
-    }
-    void unionByRank(int u,int v){
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_u==ulp_v) return;
-        if(rank[ulp_u]<rank[ulp_v]){
-            parent[ulp_u]=ulp_v;
-        }else if(rank[ulp_v]<rank[ulp_u]){
-            parent[ulp_v]=ulp_u;
-        }else{
-            parent[ulp_v]=ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-    void unionBySize(int u,int v){
-         int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_u==ulp_v) return;
-        if(size[ulp_u]<size[ulp_v]){
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-        }else{
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-    }
-};
+
 class Solution {
 public:
-    int removeStones(vector<vector<int>>& stones) {
-        int n=stones.size();
-        int maxRow=0;
-        int maxCol=0;
-        for(auto it:stones){
-            maxRow=max(maxRow,it[0]);
-            maxCol=max(maxCol,it[1]);
-        }
-        DisjointSet ds(maxRow+maxCol+1);
-        unordered_map<int,int> stoneNodes;
-        for(auto it:stones){
-            int nodeRow=it[0];
-            int nodeCol=it[1]+maxRow+1;
-            ds.unionBySize(nodeRow,nodeCol);
-            stoneNodes[nodeRow]=1;
-            stoneNodes[nodeCol]=1;
-         }
-        int cnt=0;
-        for(auto it:stoneNodes){
-            if(ds.findUPar(it.first)==it.first){
-                cnt++;
+    bool isFeasible(vector<int> &a,vector<int> &b){
+        return a[0]==b[0] || a[1]==b[1];
+    }
+    void dfs(int source,vector<vector<int>> &stones,vector<bool> &vis){
+        vis[source]=true;
+        for(int i=0;i<stones.size();i++){
+            if(vis[i]==false){
+                if(isFeasible(stones[source],stones[i])){
+                    dfs(i,stones,vis);
+                }
             }
         }
-        return n-cnt;
+        
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        int count=0;
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+            if(vis[i]==false){
+                dfs(i,stones,vis);
+                count++;
+                    
+            }
+        }
+        return n-count;
         
     }
 };
