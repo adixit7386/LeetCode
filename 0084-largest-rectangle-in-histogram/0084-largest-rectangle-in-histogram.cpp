8e3e8aea69b1546eed1556a9f6b6606e35a3699d
Abhay Dixit <adixit7386@gmail.com>
2023-04-13 22:10:39 +0530

    Time: 151 ms (93.74%), Space: 75.3 MB (93.02%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -6,10 +6,12 @@ public:
         st.push(0);
         int ans=INT_MIN;
         for(int i=1;i<n;i++){
+          
             while(st.empty()==false && heights[st.top()]>=heights[i]){
                 int index=st.top();
                 st.pop();
                 int prev=st.empty()?-1:st.top();
+                
                 ans=max(ans,heights[index]*(i-prev-1));
             }
             st.push(i);
