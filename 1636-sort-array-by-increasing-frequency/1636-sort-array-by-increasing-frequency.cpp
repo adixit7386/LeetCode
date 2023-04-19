207bad7c7406151497e6def9f69578a1ca7ff8a6
Abhay Dixit <adixit7386@gmail.com>
2023-04-19 16:05:42 +0530

    Time: 15 ms (22.92%), Space: 11.1 MB (86.36%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,21 +1,23 @@
+
+unordered_map<int,int> mp;
+  bool myComp(int a,int b){
+        if(mp[a]==mp[b]){
+            return a>b;
+        }
+        return mp[a]<mp[b];
+        
+    }
 class Solution {
 public:
     static 
-//     static bool myComp(int a,int b){
-//         if(mp[a]==mp[b]){
-//             return a>b;
-//         }
-//         return mp[a]<mp[b];
-        
-//     }
+  
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int,int> mp;
+        
         for(auto element:nums){
             mp[element]++;
         }
-        sort(nums.begin(),nums.end(),[&](int a,int b){
-            return mp[a]==mp[b] ?a>b:mp[a]<mp[b];
-        });
+        sort(nums.begin(),nums.end(),myComp);
+        mp.clear();
         return  nums;
     }
 };
