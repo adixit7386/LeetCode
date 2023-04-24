67b9355669e3f1a6e02db406cdc7bf8d9c371256
Abhay Dixit <adixit7386@gmail.com>
2023-04-24 22:47:19 +0530

    Time: 6 ms (77.84%), Space: 14.8 MB (49.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,74 +11,54 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL&&list2==NULL){
-            return NULL;
-        }
-        ListNode *head1 = list1;
-    ListNode *head2 = list2;
-    ListNode *curr = NULL;
-    ListNode *listHead;
-    while (head1 != NULL && head2 != NULL)
-    {
-        if (head1->val <= head2->val)
-        {
-            if (curr == NULL)
-            {
-                curr = head1;
-                listHead = head1;
+      ListNode* curr=NULL;
+        ListNode* head=NULL;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                if(curr==NULL){
+                    curr=list1;
+                    head=curr;
+                    
+                }else{
+                    curr->next=list1;
+                    curr=curr->next;
+                }
+                list1=list1->next;
+            }else{
+                if(curr==NULL){
+                    curr=list2;
+                    head=curr;
+                    
+                }else{
+                    curr->next=list2;
+                    curr=curr->next;
+                }
+                list2=list2->next;
             }
-            else
-            {
-                curr->next = head1;
-                curr = curr->next;
-            }
-            head1 = head1->next;
+            
         }
-        else
-        {
-            if (curr == NULL)
-            {
-                curr = head2;
-                listHead = head2;
-            }
-            else
-            {
-                curr->next = head2;
-                curr = curr->next;
-            }
-            head2 = head2->next;
+        while(list1!=NULL){
+            if(curr==NULL){
+                    curr=list1;
+                head=curr;
+                    
+                }else{
+                    curr->next=list1;
+                    curr=curr->next;
+                }
+            list1=list1->next;
         }
-    }
-
-    while (head1 != NULL)
-    {
-        if (curr == NULL)
-        {
-            curr = head1;
-            listHead=head1;
+        while(list2!=NULL){
+            if(curr==NULL){
+                    curr=list2;
+                head=curr;
+                    
+                }else{
+                    curr->next=list2;
+                    curr=curr->next;
+                }
+                list2=list2->next;
         }
-        else
-        {
-            curr->next = head1;
-                curr = curr->next;
-        }
-        head1 = head1->next;
-    }
-    while (head2 != NULL)
-    {
-        if (curr == NULL)
-        {
-            curr = head2;
-            listHead=head2;
-        }
-        else
-        {
-           curr->next = head2;
-                curr = curr->next;
-        }
-        head2 = head2->next;
-    }
-    return listHead;
-        
+        return head;
     }
 };
