ae36d0c12c114e730cc16017991f6ff3af9af24d
Abhay Dixit <adixit7386@gmail.com>
2023-04-25 21:04:48 +0530

    Time: 44 ms (56.21%), Space: 18.4 MB (33.00%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool even=false;
+    int count=0;
+    ListNode* reorder(ListNode* head,int idx,ListNode* prev){
+        if(idx==(count+1)/2){
+            if(even){
+                ListNode* temp=head->next->next;
+                head->next->next=NULL;
+                return temp;
+            }else{
+                ListNode* temp=head->next;
+                head->next=NULL;
+                return temp;
+            }
+        }
+        ListNode* newNode=reorder(head->next,idx+1,head);
+        // if(prev!=NULL)
+        // prev->next=newNode;
+        ListNode* temp=newNode->next;
+        // newNode->next=head;
+        ListNode* temp1=head->next;
+        head->next=newNode;
+        newNode->next=temp1;
+        return temp;
+        
+    }
+    void reorderList(ListNode* head) {
+       if(head==NULL || head->next==NULL)return;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            count++;
+            curr=curr->next;
+        }
+        
+        if((count)%2==0) even=true;
+        
+        reorder(head,1,NULL);
+    }
+};
