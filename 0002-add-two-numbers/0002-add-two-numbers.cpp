1c8184b0d3947359b1330c9f819e85d617f0f612
Abhay Dixit <adixit7386@gmail.com>
2023-04-25 17:57:47 +0530

    Time: 31 ms (85.71%), Space: 71.5 MB (55.57%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        if(head==NULL || head->next==NULL)return head;
+        ListNode* next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* list1, ListNode* list2) {
+        // ListNode* list1=reverseList(l1);
+        // ListNode* list2=reverseList(l2);
+        int ans=0;
+        int carry=0;
+        ListNode* head=NULL;
+        ListNode* headStart=NULL;
+        while(list1!=NULL && list2!=NULL){
+            int currSum= list1->val + list2->val + carry;
+            // ans= ans*10 + (currSum%10);
+            if(head==NULL){
+                head=new ListNode(currSum%10);
+                headStart=head;
+            }
+            else {
+                head->next=new ListNode(currSum%10);
+                head=head->next;
+            }
+            carry=currSum/10;
+            list1=list1->next;
+            list2=list2->next;
+        }
+        while(list1!=NULL){
+            int currSum=list1->val+carry;
+            // ans= ans*10 + (currSum%10);
+            if(head==NULL){
+                
+                head=new ListNode(currSum%10);
+                headStart=head;
+            }
+            else {head->next=new ListNode(currSum%10);
+                 head=head->next;}
+            carry=currSum/10;
+            list1=list1->next;
+        }
+        while(list2!=NULL){
+            int currSum=list2->val+carry;
+            if(head==NULL){head=new ListNode(currSum%10);
+                          headStart=head;}
+            else {head->next=new ListNode(currSum%10);
+                 head=head->next;}
+            // ans= ans*10 + (currSum%10);
+            carry=currSum/10;
+            list2=list2->next;
+        }
+        if(carry!=0){
+            head->next= new ListNode(carry);
+        }
+        return headStart;
+        
+    }
+};
