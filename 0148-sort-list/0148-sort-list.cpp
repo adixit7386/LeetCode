64614136caee5a6de5d0c493eac3f4dcfb21fbc7
Abhay Dixit <adixit7386@gmail.com>
2023-04-26 21:22:05 +0530

    Time: 185 ms (90.41%), Space: 50.8 MB (98.28%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+      ListNode* curr=NULL;
+        ListNode* head=NULL;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                if(curr==NULL){
+                    curr=list1;
+                    head=curr;
+                    
+                }else{
+                    curr->next=list1;
+                    curr=curr->next;
+                }
+                list1=list1->next;
+            }else{
+                if(curr==NULL){
+                    curr=list2;
+                    head=curr;
+                    
+                }else{
+                    curr->next=list2;
+                    curr=curr->next;
+                }
+                list2=list2->next;
+            }
+            
+        }
+        while(list1!=NULL){
+            if(curr==NULL){
+                    curr=list1;
+                head=curr;
+                    
+                }else{
+                    curr->next=list1;
+                    curr=curr->next;
+                }
+            list1=list1->next;
+        }
+        while(list2!=NULL){
+            if(curr==NULL){
+                    curr=list2;
+                head=curr;
+                    
+                }else{
+                    curr->next=list2;
+                    curr=curr->next;
+                }
+                list2=list2->next;
+        }
+        return head;
+    }
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL)return NULL;
+        
+        ListNode* fastPtr=head->next;
+        ListNode* slowPtr=head;
+        
+        while(fastPtr!=NULL && fastPtr->next!=NULL){
+            fastPtr=fastPtr->next->next;
+            slowPtr=slowPtr->next;
+        }
+        return slowPtr;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL)return head;
+        ListNode* mid=middleNode(head);
+        ListNode* list1=head;
+        
+        ListNode* list2=mid->next;
+        mid->next=NULL;
+        list1=sortList(list1);
+        list2=sortList(list2);
+        return mergeTwoLists(list1,list2);
+        
+    }
+};
