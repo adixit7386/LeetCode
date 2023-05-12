8d94bec650f007717464b4e89dd88910c6b8666e
Abhay Dixit <adixit7386@gmail.com>
2023-05-12 23:34:15 +0530

    Time: 5 ms (28.53%), Space: 6.1 MB (77.73%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    int countWays(string s,int i,int taken,vector<vector<int>>& dp){
-        if(i>=s.length()){
-            return 1;
-        }
-        int count=0;
-        if(dp[taken][i]==-1){
-            
-        
-        if(!taken){
-            int valid=(s[i]-'0')+(s[i-1]-'0')*10;
-            if(valid>0 && valid<27){
-                count+=countWays(s,i+1,1,dp);
-            }
-            
-        }else{
-            //include current character 
-            
-            int valid=s[i]-'0';
-            if(valid>0){
-                count+=countWays(s,i+1,1,dp);
-            }else{
-               dp[taken][i]=count;
-                return dp[taken][i];
-            }
-            
-            
-            //skip current character
-            if(i<s.length()-1)
-            count+=countWays(s,i+1,0,dp);
-            
-            
-        }
-             dp[taken][i]=count;
-            
-        }
-       
-        return dp[taken][i];
-        
+    bool isValid(string str){
+        if(str.length()==0)return false;
+        if(str[0]=='0')return false;
+        if(stoi(str)<=26)return true;
+        return false;
+    }
+    int decodeWays(string s,int i){
+        if(i>=s.length())return 1;
+        //of digit 1;
+        string str="";
+        str+=s[i];
+        int ans=0;
+        if(isValid(str))
+            ans=decodeWays(s,i+1);
+        if(i>=s.length()-1)return ans;
+        str+=s[i+1];
+        if(isValid(str))
+            return ans+decodeWays(s,i+2);
+        return ans;
+
+        //of digit 2
+
+
     }
     int numDecodings(string s) {
-        vector<vector<int>> dp(2,vector<int> (s.size()+1,-1));
-        dp[0][s.size()]=1;
-        dp[1][s.size()]=1;
-        
-        
-        return countWays(s,0,1,dp);
-        
+        int n=s.length();
+        vector<int> dp(n+2,0);
+        dp[n]=1;
+        dp[n+1]=1;
+        for(int i=n-1;i>=0;i--){
+            string str="";
+            str+=s[i];
+            int ans=0;
+            if(isValid(str))
+                ans=dp[i+1];
+            if(i>=n-1){
+                dp[i]=ans;
+                continue;
+            }
+            str+=s[i+1];
+            if(isValid(str))
+                dp[i]=ans+dp[i+2];
+            else
+            dp[i]=ans;
+                
+        }
+        return dp[0];
     }
 };
